77f4421fdbb34aeaeb711f31d14392c163f0bb27
Rahul Chanana <eemaanaman@gmail.com>
2021-07-24 15:08:33 +0530

    Complete 5 rounds game

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,67 @@
-function playRound(playerSelection, computerSelection) {
-    switch (true) {
-        case playerSelection == "rock" && computerSelection == "paper":
-        case playerSelection == "paper" && computerSelection == "scissors":
-        case playerSelection == "scissors" && computerSelection == "rock":
-            return "Result: You lost";
-            break;
-        case playerSelection == "rock" && computerSelection == "scissors":
-        case playerSelection == "paper" && computerSelection == "rock":
-        case playerSelection == "scissors" && computerSelection == "paper":
-            return "Result: You won";
-            break;
-        case playerSelection == "rock" && computerSelection == "rock":
-        case playerSelection == "paper" && computerSelection == "paper":
-        case playerSelection == "scissors" && computerSelection == "scissors":
-            return "Result: It's a tie";
-            break;
-        default:
-            return "Result: Invalid argument";
+let playerScore = 0;
+let computerScore = 0;
+
+for (let i = 1; i <= 5; i++) {
+
+    const playerSelection = prompt("Round " + i + ": Rock, Paper or Scissors?");
+    const computerSelection = computerPlay(Math.floor(Math.random()*3));
+
+    function computerPlay(x) {
+        switch(x) {
+            case 0:
+                return "rock";
+                break;
+            case 1:
+                return "paper";
+                break;
+            case 2:
+                return "scissors";
+        }
     }
+
+    playRound(playerSelection.toLowerCase(), computerSelection);
+
+    function playRound(playerSelection, computerSelection) {
+        switch (true) {
+            case playerSelection == "rock" && computerSelection == "paper":
+            case playerSelection == "paper" && computerSelection == "scissors":
+            case playerSelection == "scissors" && computerSelection == "rock":
+                computerScore++;
+                console.log("Your selection: " + playerSelection.toLowerCase() + 
+                ", Computer's selection: " + computerSelection);
+                console.log("Your Score: " + playerScore + 
+                ", Computer's score: " + computerScore);
+                break;
+            case playerSelection == "rock" && computerSelection == "scissors":
+            case playerSelection == "paper" && computerSelection == "rock":
+            case playerSelection == "scissors" && computerSelection == "paper":
+                playerScore++;
+                console.log("Your selection: " + playerSelection.toLowerCase() + 
+                ", Computer's selection: " + computerSelection);
+                console.log("Your Score: " + playerScore + 
+                ", Computer's score: " + computerScore);
+                break;
+            case playerSelection == "rock" && computerSelection == "rock":
+            case playerSelection == "paper" && computerSelection == "paper":
+            case playerSelection == "scissors" && computerSelection == "scissors":
+                computerScore++, playerScore++;
+                console.log("Your selection: " + playerSelection.toLowerCase() + 
+                ", Computer's selection: " + computerSelection);
+                console.log("Your Score: " + playerScore + 
+                ", Computer's score: " + computerScore);
+                break;
+            default:
+                i--;
+                alert("Invalid argument");
+        }
+    }
+
 }
 
-function computerPlay(x) {
-    switch(x) {
-        case 0:
-            return "rock";
-            break;
-        case 1:
-            return "paper";
-            break;
-        case 2:
-            return "scissors";
+    if (playerScore > computerScore) {
+        console.log("You've won the game");
+    } else if (computerScore > playerScore) {
+        console.log("You've lost the game");
+    } else {
+        console.log("It's a draw");
     }
-}
-
-const playerSelection = prompt("Rock, Paper or Scissors?");
-const computerSelection = computerPlay(Math.floor(Math.random()*3));
-console.log("Your selection: " + playerSelection.toLowerCase());
-console.log("Computer's selection: " + computerSelection);
-console.log(playRound(playerSelection.toLowerCase(), computerSelection));
